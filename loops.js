3d1cf35d4b4ad166532b2b19b532180aea8b59b7
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-03-01 15:28:53 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,5 +1,5 @@
 function forLoop(array) {
-  for(let i = 0; i < array.length; i++) {
+  for(let i = 0; i < 58; i++) {
     if(i == 1) {
       // array.push("I am 1 strange loop.");
       array[i] = "I am 1 strange loop.";
